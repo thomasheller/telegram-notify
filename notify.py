dbb9bfa5e82524e3c65b4182602b6d93f80b50aa
Thomas Heller <thomas.m.heller@gmail.com>
2020-04-08 21:34:57 +0200

    trim long messages

--- notify.py
+++ notify.py
@@ -8,6 +8,10 @@ TELEGRAM_APITOKEN='<your_api_token_here>'
 TELEGRAM_CHATID='<your_chat_id_here>'
 
 message = sys.stdin.read()
+
+if len(message) > 4096:
+    message = message[:4092] + ' ...'
+
 query = urllib.quote(message)
 
 result = requests.get('https://api.telegram.org/bot' + TELEGRAM_APITOKEN + '/sendMessage?chat_id=' + TELEGRAM_CHATID + '&text=' + query)
